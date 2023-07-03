4e8f32e8012795455d9eab39db5d0d54973b07ae
Rob Weber <robweberjr@gmail.com>
2023-07-03 13:30:27 -0500

    make community arg -C for consistancy

--- check_snmp.py
+++ check_snmp.py
@@ -9,7 +9,7 @@ from hnmp import SNMP, SNMPError
 # parse the arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-H', '--host', required=True, help='The host address')
-parser.add_argument('-c', '--community', required=True, help='The community string')
+parser.add_argument('-C', '--community', required=True, help='The community string')
 parser.add_argument('-o', '--oid', required=True, help='The oid to check')
 
 args = parser.parse_args()
